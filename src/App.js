c540bc2fd90e5a7b62e2d747e192eae856b895bb
Roger Vera <rovera@vassar.edu>
2019-01-20 18:43:43 -0500

    stub redirect page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   render() {
     return (
       <>
-      <WelcomePrompt/>
+      <Route exact path='/authorized' component={RedirectPage} />
+      <Route exact path='/' component={WelcomePrompt} />
       </>
     );
   }
