d53e2cdbdaf4bcd45c5928651f5a4fa084cd48e5
Roger Vera <rovera@vassar.edu>
2019-01-20 15:30:10 -0500

    install and import react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+
+/* React Router */
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+
+/* Redux Configuration */
+// import { Provider } from 'react-redux'
+// import { store } from './store.js'
+
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Router>
+      <Route path='/' component={App} />
+    </Router>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
