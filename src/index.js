96f73d483b1bdc13c4c623eac50429120c865412
Roger Vera <rovera@vassar.edu>
2019-01-21 18:27:14 -0500

    stub state saving

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Startup from './Startup'
 import App from './App';
 
 /* React Router */
