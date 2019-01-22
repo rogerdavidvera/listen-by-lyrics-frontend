333a1ed37563ace8d27a3914d398f621f26b3317
Roger Vera <rovera@vassar.edu>
2019-01-22 01:11:12 -0500

    stub css import

--- src/loggedIn/MainPage.js
+++ src/loggedIn/MainPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { Container, Row, Col } from 'reactstrap';
+// import '../styles/MainPage.css';
 
 const MainPage = ({ user }) => (
   <>
-    <h1>Hi, {user.display_name}!</h1>
-    <img src={user.img_url} alt={user.display_name + 's profile image'}/>
+    <Container className='MainPage'>
+      <h1>Hi, {user.display_name}!</h1>
+      <img src={user.img_url} alt={user.display_name + 's profile image'}/>
+    </Container>
   </>
 )
 
