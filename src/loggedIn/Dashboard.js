26462d04e3e154e4cf5b3fd659131be8ee18d23b
Roger Vera <rovera@vassar.edu>
2019-01-22 01:05:18 -0500

    import main page

--- src/loggedIn/Dashboard.js
+++ src/loggedIn/Dashboard.js
@@ -2,21 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 import NavBar from './navigation/NavBar'
+import MainPage from './MainPage'
 
 const Dashboard = ({ user }) => (
   <div className="homepage">
     <NavBar/>
-    <h1>{user.display_name}</h1>
-    <img src={user.img_url} alt={user.display_name + 's profile image'}/>
-    <br/>
-    <br/>
+    <MainPage />
   </div>
 )
 
-const mapStateToProps = (state) => {
-  return {
-    user: state.auth.user
-  }
-}
-
-export default connect(mapStateToProps)(Dashboard)
+export default Dashboard
