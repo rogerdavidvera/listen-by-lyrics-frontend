f9d3f0a78e353a80e79c031536332180fdabb38d
Roger Vera <rovera@vassar.edu>
2019-01-21 19:48:42 -0500

    stub page

---
+++ src/loggedIn/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import LogoutButton from './LogoutButton';
+
+const WelcomePrompt = () => (
+  <div className="homepage">
+    <h1>{this.props.auth.user.display_name}</h1>
+    <btn>Hi</btn>
+  </div>
+)
+
+export default WelcomePrompt
