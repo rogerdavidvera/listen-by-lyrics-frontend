c14e972b7e05eedd47a3be2c69e4348a6835f068
Roger Vera <rovera@vassar.edu>
2019-01-21 20:01:22 -0500

    add log out button

--- src/loggedIn/Dashboard.js
+++ src/loggedIn/Dashboard.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux'
-// import LogoutButton from './LogoutButton';
+import LogoutButton from './LogoutButton';
 
 const Dashboard = ({ user }) => (
   <div className="homepage">
     <h1>{user.display_name}</h1>
     <img src={user.img_url} alt={user.display_name + 's profile image'}/>
+    <br/>
+    <br/>
+    <LogoutButton />
   </div>
 )
 
