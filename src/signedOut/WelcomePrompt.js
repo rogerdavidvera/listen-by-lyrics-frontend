0ec6e5ddde8abb1e2c72807779dc685db24ecf10
Roger Vera <rovera@vassar.edu>
2019-01-21 22:00:33 -0500

    map state to props

--- src/signedOut/WelcomePrompt.js
+++ src/signedOut/WelcomePrompt.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import '../styles/WelcomePrompt.css';
 import { Jumbotron } from 'reactstrap';
 import LoginButton from './LoginButton';
 
-const WelcomePrompt = () => (
+const WelcomePrompt = ({ rehydrated }) => {
+  console.log(rehydrated)
+  return (
   <div className="homepage">
-    <Jumbotron>
-      <div className="welcomeContent">
-        <h1 className="title">ListenByLyrics</h1>
-        <p className="subtitle">Search for songs by lyrics, and listen to them instantly</p>
-        <hr className="my-2" />
-        <p className="login-prompt"></p>
-        <p className="lead">
-          <LoginButton />
-        </p>
-      </div>
-    </Jumbotron>
+  <Jumbotron>
+    <div className="welcomeContent">
+      <h1 className="title">ListenByLyrics</h1>
+      <p className="subtitle">Search for songs by lyrics, and listen to them instantly</p>
+      <hr className="my-2" />
+      <p className="login-prompt"></p>
+      <p className="lead">
+        <LoginButton />
+      </p>
+    </div>
+  </Jumbotron>
   </div>
-)
+  )
+}
 
-export default WelcomePrompt
+const mapStateToProps = (state) => {
+  return {
+    rehydrated: state._persist.rehydrated
+  }
+}
+
+export default connect(mapStateToProps)(WelcomePrompt)
