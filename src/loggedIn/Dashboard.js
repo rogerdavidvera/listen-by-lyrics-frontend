20d88da082a85eaef51044b978058df3bae49732
Roger Vera <rovera@vassar.edu>
2019-01-22 01:38:05 -0500

    background only for nav bar

--- src/loggedIn/Dashboard.js
+++ src/loggedIn/Dashboard.js
@@ -5,10 +5,10 @@ import NavBar from './navigation/NavBar'
 import MainPage from './MainPage'
 
 const Dashboard = ({ user }) => (
-  <div className="homepage">
+  <>
     <NavBar/>
     <MainPage />
-  </div>
+  </>
 )
 
 export default Dashboard
