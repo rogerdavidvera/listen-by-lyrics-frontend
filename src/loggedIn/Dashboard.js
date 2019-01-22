a32303ae04e98b63c8f5d87972e88040b3424c33
Roger Vera <rovera@vassar.edu>
2019-01-21 20:02:38 -0500

    remove image for testing

--- src/loggedIn/Dashboard.js
+++ src/loggedIn/Dashboard.js
@@ -5,7 +5,6 @@ import LogoutButton from './LogoutButton';
 const Dashboard = ({ user }) => (
   <div className="homepage">
     <h1>{user.display_name}</h1>
-    <img src={user.img_url} alt={user.display_name + 's profile image'}/>
     <br/>
     <br/>
     <LogoutButton />
@@ -17,5 +16,6 @@ const mapStateToProps = (state) => {
     user: state.auth.user
   }
 }
+// <img src={user.img_url} alt={user.display_name + 's profile image'}/>
 
 export default connect(mapStateToProps)(Dashboard)
