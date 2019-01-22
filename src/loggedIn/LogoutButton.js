9462b72ce66e2f52c9805f29700d663b4166f768
Roger Vera <rovera@vassar.edu>
2019-01-21 20:13:13 -0500

    import logout action

--- src/loggedIn/LogoutButton.js
+++ src/loggedIn/LogoutButton.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import { Button } from 'reactstrap';
-
+import { logoutUser } from '../actions/AuthActions';
 
 const LogoutButton = () => (
   <Button outline color="light" onClick={() => alert('Logging Out')}>Log Out</Button>
