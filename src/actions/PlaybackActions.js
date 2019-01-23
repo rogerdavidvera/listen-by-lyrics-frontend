49b212c0d9d7865dee444c5c55c969cfa6b9c603
Roger Vera <rovera@vassar.edu>
2019-01-22 19:31:12 -0500

    stub play action

---
+++ src/actions/PlaybackActions.js
@@ -0,0 +1,17 @@
+import API_URL from '../assets/ApiUrl'
+import HEADERS from '../assets/AuthorizationHeaders'
+import { PLAY_SONG } from './types'
+
+export function playSong(song){
+  return(dispatch) => {
+    return fetch(`${API_URL}play`, {
+      method: 'POST',
+      headers: HEADERS,
+      body: JSON.stringify({track_uri: song.track_id, lyrics_url: song.lyrics_url})
+    })
+    .then(response => response.json())
+    .then(data => {
+      return dispatch({type: PLAY_SONG, payload: {current_song: song, lyrics: data.lyrics}})
+    })
+  }
+}
