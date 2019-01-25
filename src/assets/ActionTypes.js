6942ee1ca555fc41f4a10730a92a20bdb8cad2f2
Roger Vera <rovera@vassar.edu>
2019-01-25 16:08:17 -0500

    deploy ready

--- src/assets/ActionTypes.js
+++ src/assets/ActionTypes.js
@@ -1,4 +1,5 @@
 export const LOGOUT_USER = 'LOGOUT_USER'
+export const LOGGING_IN = 'LOGGING_IN'
 export const UPDATE_AUTHORIZATION = 'UPDATE_AUTHORIZATION'
 export const PLAY_SONG = 'PLAY_SONG'
 export const SAVE_SONG = 'SAVE_SONG'
