48a5f1bfd27dd03907b5ca7d05e7fe0b7647e7b3
Roger Vera <rovera@vassar.edu>
2019-01-25 18:26:12 -0500

    fix

--- src/signedOut/MadeWithLove.js
+++ src/signedOut/MadeWithLove.js
@@ -4,7 +4,7 @@ import { Container, Divider } from 'semantic-ui-react'
 const MadeWithLove = () => (
   <>
   <Container className="MadeWithLove">
-  <Divider hidden />
+  <Divider hidden/>
   Made with <span className="emoji" role="img" aria-label="Heart">❤️</span> by <a className="name" href="https://github.com/rogerdavidvera" target="_blank" rel="noopener noreferrer">Roger Vera</a>
   </Container>
   </>
