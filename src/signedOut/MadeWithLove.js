f740c8f5567e44e1aa10ee154f6563defd25e4bc
Roger Vera <rovera@vassar.edu>
2019-03-21 22:46:33 -0400

    fix github URL

--- src/signedOut/MadeWithLove.js
+++ src/signedOut/MadeWithLove.js
@@ -5,7 +5,7 @@ const MadeWithLove = () => (
   <>
   <Container className="MadeWithLove">
   <Divider hidden />
-  Made with <span className="emoji" role="img" aria-label="Heart">❤️</span> by <a className="name" href="https://github.com/rogerdavidvera" target="_blank" rel="noopener noreferrer">Roger Vera</a>
+  Made with <span className="emoji" role="img" aria-label="Heart">❤️</span> by <a className="name" href="https://github.com/rogervera" target="_blank" rel="noopener noreferrer">Roger Vera</a>
   </Container>
   </>
 )
